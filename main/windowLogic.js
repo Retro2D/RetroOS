4d9cd76e64f5bbfce2b68d058fc61899f9019739
Retro2D <longdaniel788@gmail.com>
2026-06-30 03:37:48 +0000

    First attempt to implement window dragging

--- main/windowLogic.js
+++ main/windowLogic.js
@@ -1,1 +1,42 @@
 dragElement(document.getElementById("welcomeWindow"))
+
+function dragElement(element) {
+    var initialX = 0, initialY = 0, currentX = 0, currentY = 0;
+
+    if (document.getElementById(element.id + "header")) {
+        document.getElementById(element.id + "header").onmousedown = startDrag;
+
+    } else {
+        element.onmousedown = startDrag;
+    }
+
+    function startDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+
+        initialX = e.clientX;
+        initialY = e.clientY;
+
+        document.onmouseup = stopDrag;
+        document.onmousemove = DragElement
+
+        }
+
+    function DragElement(e) {
+        e = e || window.event;
+        e.preventDefault();
+
+        currentX = initialX - e.clientX;
+        currentY = initialY - e.clientY;
+        initialX = e.clientX;
+        initialY = e.clientY;
+
+        element.style.top = (element.offsetTop - currentY) + "px";
+        element.style.left = (element.offsetTop - currentX) + "px";
+    }
+
+    function stopDrag() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
