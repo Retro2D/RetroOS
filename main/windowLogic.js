bc0e8ab665bcc860688f1486ccb638d9cc60596e
Retro2D <longdaniel788@gmail.com>
2026-07-18 17:35:09 +0000

    Changed windowLogic to check if element exists, position of welcomeTopBar = absolute

--- main/windowLogic.js
+++ main/windowLogic.js
@@ -1,43 +1,45 @@
 dragElement(document.getElementById("welcomeWindow"))
 
 function dragElement(element) {
-    var initialX = 0, initialY = 0, currentX = 0, currentY = 0;
+    if (element !==undefined && element !==null) {
+        var initialX = 0, initialY = 0, currentX = 0, currentY = 0;
 
-    if (document.getElementById(element.id + "header")) {
-        document.getElementById(element.id + "header").onmousedown = startDrag;
+        if (document.getElementById(element.id + "header")) {
+            document.getElementById(element.id + "header").onmousedown = startDrag;
 
-    } else {
-        element.onmousedown = startDrag;
-    }
-
-    function startDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-
-        initialX = e.clientX;
-        initialY = e.clientY;
-
-        document.onmouseup = stopDrag;
-        document.onmousemove = DragElement;
-
+        } else {
+            element.onmousedown = startDrag;
         }
 
-    function DragElement(e) {
-        e = e || window.event;
-        e.preventDefault();
+        function startDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
 
-        currentX = initialX - e.clientX;
-        currentY = initialY - e.clientY;
-        initialX = e.clientX;
-        initialY = e.clientY;
+            initialX = e.clientX;
+            initialY = e.clientY;
 
-        element.style.top = (element.offsetTop - currentY) + "px";
-        element.style.left = (element.offsetLeft - currentX) + "px";
-    }
+            document.onmouseup = stopDrag;
+            document.onmousemove = DragElement;
 
-    function stopDrag() {
-        document.onmouseup = null;
-        document.onmousemove = null;
+            }
+
+        function DragElement(e) {
+            e = e || window.event;
+            e.preventDefault();
+
+            currentX = initialX - e.clientX;
+            currentY = initialY - e.clientY;
+            initialX = e.clientX;
+            initialY = e.clientY;
+
+            element.style.top = (element.offsetTop - currentY) + "px";
+            element.style.left = (element.offsetLeft - currentX) + "px";
+        }
+
+        function stopDrag() {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
     }
 }
 
