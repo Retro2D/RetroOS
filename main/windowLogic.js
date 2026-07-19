97b4ae1ddbcc0c5bbad27c35b4ba48798cd5323f
Retro2D <longdaniel788@gmail.com>
2026-07-19 21:23:48 +0000

    Fixed openWindow styles to match the windows

--- main/windowLogic.js
+++ main/windowLogic.js
@@ -87,7 +87,7 @@ function closeWindow(element) {
 }
 
 function openWindow(element) {
-    element.style.display = "flex";
+    element.style.display = "inline";
 }
 
 if (welcomeClose !==undefined && welcomeClose !==null) {
